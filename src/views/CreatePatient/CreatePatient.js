2bf3ec8448527e1e64f449cf2a8ec6bddec35cd8
diamantisufi <diamant.isufi@atis.al>
2021-04-22 10:59:52 +0200

    wip create-patient view

--- src/views/CreatePatient/CreatePatient.js
+++ src/views/CreatePatient/CreatePatient.js
@@ -1,7 +1,124 @@
-import React from "react";
-
+import {
+  Box,
+  Container,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+  Tooltip,
+  Button,
+} from "@material-ui/core";
+import { Close, KeyboardBackspaceOutlined } from "@material-ui/icons";
+import React, { Fragment } from "react";
+import styles from "./CreatePatient.module.scss";
 const CreatePatient = () => {
-  return <div>Create Patient Page</div>;
+  return (
+    <Fragment>
+      <Container maxWidth="xl">
+        <Box mb={3}>
+          <Tooltip title="Back">
+            <Button startIcon={<KeyboardBackspaceOutlined />}> Back</Button>
+          </Tooltip>
+        </Box>
+
+        <Paper>
+          <Box className={styles.header}>
+            <Typography variant="h6" color="textSecondary">
+              Create new Patient
+            </Typography>
+          </Box>
+          <Grid container>
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              padding="1em"
+              pt="2em"
+              width="100%"
+              gridRowGap="1"
+            >
+              <Grid item xs="6" className={styles.grid}>
+                <Box className={styles.textfield}>
+                  <TextField
+                    id="firstname"
+                    name="firstname"
+                    label="First Name"
+                    variant="outlined"
+                    fullWidth
+                  ></TextField>
+                </Box>
+                <Box className={styles.textfield}>
+                  <TextField
+                    id="firstname"
+                    name="firstname"
+                    label="Date of Birth"
+                    variant="outlined"
+                    fullWidth
+                  ></TextField>
+                </Box>
+                <Box className={styles.textfield}>
+                  <TextField
+                    id="firstname"
+                    name="firstname"
+                    label="Telephone"
+                    variant="outlined"
+                    fullWidth
+                  ></TextField>
+                </Box>
+
+                <Box className={styles.textfield}>
+                  <TextField
+                    id="firstname"
+                    name="firstname"
+                    label="Residency"
+                    variant="outlined"
+                    fullWidth
+                  ></TextField>
+                </Box>
+              </Grid>
+              <Grid item xs="6">
+                <Box className={styles.textfield}>
+                  <TextField
+                    id="firstname"
+                    name="firstname"
+                    label="Last Name"
+                    variant="outlined"
+                    fullWidth
+                  ></TextField>
+                </Box>
+                <Box className={styles.textfield}>
+                  <TextField
+                    id="firstname"
+                    name="firstname"
+                    label="Place of Birth"
+                    variant="outlined"
+                    fullWidth
+                  ></TextField>
+                </Box>
+                <Box className={styles.textfield}>
+                  <TextField
+                    id="firstname"
+                    name="firstname"
+                    label="Date of Birth"
+                    variant="outlined"
+                    fullWidth
+                  ></TextField>
+                </Box>
+                <Box className={styles.textfield}>
+                  <TextField
+                    id="firstname"
+                    name="firstname"
+                    label="Telephone"
+                    variant="outlined"
+                    fullWidth
+                  ></TextField>
+                </Box>
+              </Grid>
+            </Box>
+          </Grid>
+        </Paper>
+      </Container>
+    </Fragment>
+  );
 };
 
 export default CreatePatient;
